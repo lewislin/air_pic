4f2df64bc8359b74d5514c1a7a68ebb2d9234ea1
Lewis Lin <lewilin@yahoo.com>
2012-02-18 08:17:25 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AirPic::Application.config.secret_token = '523dec8e1b8df04a525932c09edce07fdc4ac0310ab6b1aa5f3bea8ddc2ef613aade6f7b8126ed4cab9764084897668de19339b3c2432123924add14a9fa1d4b'
